9a059a4425d8a3488a581ccf6a4f9304425a4732
Dimas Raka Septiawan <dimas.raka@mekari.com>
2024-07-29 14:16:24 +0700

    feat: update experiment component

--- components/content/ExperimentFeatured.vue
+++ components/content/ExperimentFeatured.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+  title: String,
+  description: String,
+  image: String,
+  showData: Boolean
+});
+</script>
 
 <template>
   <div data-id="experiment">
+    <div>
+      <ul>
+        <li>
+          <span>Experiment Title:</span> {{ title }}
+        </li>
+        <li>
+          <span>Experiment Description:</span> {{ description }}
+        </li>
+        <li>
+          <img :src="image" alt="Experiment Image" />
+        </li>
+        <li>
+          <span v-if="showData">Show Data</span>
+        </li>
+      </ul>
+    </div>
     <h3 data-id="title">
       <ContentSlot name="title" />
     </h3>
